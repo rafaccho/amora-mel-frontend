d8d0c79f534276a710e5f40de35ef5a3119b3dfe
DSRafacho <davi.s.rafacho.developer@gmail.com>
2022-07-22 14:18:46 -0300

    Fixação do footer e do cabeçalho do card

--- src/componentes/CardAreaEntrega/index.tsx
+++ src/componentes/CardAreaEntrega/index.tsx
@@ -7,12 +7,12 @@ export function CardAreaEntrega(props: {
     dados: any,
 }) {
     return (
-        <div id="card" className="w-full h-52 bg-orange-400 rounded-md overflow-y-auto">
-            <div id="card-header" className="bg-orange-600 rounded-md text-center font-bold py-1">
+        <div id="card" className="w-full h-96 bg-orange-400 rounded-md overflow-y-auto">
+            <div id="card-header" className="bg-orange-600 rounded-md text-center font-bold py-1 sticky top-0">
                 {props.titulo}
             </div>
 
-            <div id="card-body" className="p-4">
+            <div id="corpo-card" className="p-4">
 
                 {
                     Object.keys(props.dados).map((key: string, index: number) => (
@@ -25,11 +25,16 @@ export function CardAreaEntrega(props: {
                     ))
                 }
 
-                <div className="w-full flex justify-center">
-                    <Button className="btn-l font-bold" title={"Editar"} onClick={() => props.onEditar()} />
-                </div>
+
 
             </div>
+            <div id="rodape-card" className="p-4 bg-orange-600 sticky bottom-0">
+                <div className="w-full flex justify-center gap-6">
+                    <Button className="btn-l font-bold" title={"Editar"} onClick={() => props.onEditar()} />
+                    <Button className="btn-l font-bold" title={"Deletar"} onClick={() => props.onEditar()} />
+                </div>
+            </div>
+
         </div>
     )
 }
