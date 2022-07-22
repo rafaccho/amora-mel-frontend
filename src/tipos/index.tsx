8cd8de52d971d1a1e69a5360dd679e8780b0914b
DSRafacho <davi.s.rafacho.developer@gmail.com>
2022-07-22 15:17:40 -0300

    Adição do tipo ModuloQuery para a query do react query

--- src/tipos/index.tsx
+++ src/tipos/index.tsx
@@ -1,7 +1,8 @@
 export type MetodoHttp = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH'
-export type Endpoint = 'pedidos' | 'areas_entregas' | 'fonecedores' | 'lojas' | 'produtos' | 'produto_fornecedores' | 'compras' | 'areas_entregas_produto'
+export type Endpoint = 'pedidos' | 'areas_entregas' | 'fornecedores' | 'lojas' | 'produtos' | 'produto_fornecedores' | 'compras' | 'areas_entregas_produto'
 export type PedidoStatus = 'A' | 'P' | 'B' | 'F' | 'C' 
 export type UnidadeLoja = 'FA' | 'CN' | 'UN' | 'FL'
 export type SimNao = 'S' | 'N'
 export type Estado = 'RO' | 'AC' | 'AM' | 'RR' | 'PA' | 'AP' | 'TO' | 'MA' | 'PI' | 'CE' | 'RN' | 'PB' | 'PE' | 'AL' | 'SE' | 'BA' | 'MG' | 'ES' | 'RJ' | 'SP' | 'PR' | 'SC' | 'RS' | 'MS' | 'MT' | 'GO' | 'DF'
 export type ReponseStatus = 200 | 201 | 400 | 404 | 500
+export type QueryModulo = "areasEntregas" | "produtos"
