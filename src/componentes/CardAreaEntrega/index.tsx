2c5aa713d8b783b27cae10d2f8700630f12ec6a9
DSRafacho <davi.s.rafacho.developer@gmail.com>
2022-07-22 14:27:58 -0300

    Adição dos props de endpoint e uuid no componente de card

--- src/componentes/CardAreaEntrega/index.tsx
+++ src/componentes/CardAreaEntrega/index.tsx
@@ -1,37 +1,38 @@
 import { Button } from "../../tags";
+import { useBackend } from "../../hooks/useBackend";
+import { Endpoint } from "../../tipos";
 
 export function CardAreaEntrega(props: {
     onEditar: () => void,
-    // onSalvar: () => void,
     titulo: any,
     dados: any,
+    endpoint: Endpoint,
+    uuid: string,
 }) {
+    const { deletarRegistro } = useBackend(props.endpoint)
     return (
         <div id="card" className="w-full h-96 bg-orange-400 rounded-md overflow-y-auto">
-            <div id="card-header" className="bg-orange-600 rounded-md text-center font-bold py-1 sticky top-0">
+            <div id="card-header" className="bg-orange-700 rounded-md text-center font-bold py-1 sticky top-0">
                 {props.titulo}
             </div>
 
             <div id="corpo-card" className="p-4">
-
                 {
-                    Object.keys(props.dados).map((key: string, index: number) => (
-                        <div key={key} className="flex flex-col mb-4">
-                            <b>{key}</b>
+                    Object.keys(props.dados).map((chave: string, index: number) => (
+                        <div key={chave} className="flex flex-col mb-4">
+                            <b>{chave}</b>
 
                             <div className="w-full p-1 bg-orange-700 rounded-lg mb-1" />
                             <span>{Object.values(props.dados)[index] as string}</span>
                         </div>
                     ))
                 }
-
-
-
             </div>
-            <div id="rodape-card" className="p-4 bg-orange-600 sticky bottom-0">
+
+            <div id="rodape-card" className="p-4 bg-orange-700 sticky bottom-0">
                 <div className="w-full flex justify-center gap-6">
                     <Button className="btn-l font-bold" title={"Editar"} onClick={() => props.onEditar()} />
-                    <Button className="btn-l font-bold" title={"Deletar"} onClick={() => props.onEditar()} />
+                    <Button className="btn-l font-bold" title={"Deletar"} onClick={() => deletarRegistro(props.uuid)} />
                 </div>
             </div>
 
