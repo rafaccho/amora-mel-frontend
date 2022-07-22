3a19d2d018786b906fc54dc265c5ed7b8dbfe326
DSRafacho <davi.s.rafacho.developer@gmail.com>
2022-07-22 15:17:22 -0300

    Adição do botão e da lógica de deleção

--- src/componentes/CardAreaEntrega/index.tsx
+++ src/componentes/CardAreaEntrega/index.tsx
@@ -1,27 +1,40 @@
+import { useMutation, useQueryClient } from "react-query";
+import { toast } from "react-toastify";
+
 import { Button } from "../../tags";
 import { useBackend } from "../../hooks/useBackend";
-import { Endpoint } from "../../tipos";
+import { Endpoint, QueryModulo } from "../../tipos";
+import { DEFAULT_TOAST_CONFIG } from "../../constantes";
 
-export function CardAreaEntrega(props: {
+export function CardRegistros(props: {
     onEditar: () => void,
     titulo: any,
     dados: any,
     endpoint: Endpoint,
+    textosDeletar: {erro: string, sucesso: string},
     uuid: string,
+    query: QueryModulo,
 }) {
     const { deletarRegistro } = useBackend(props.endpoint)
+    const queryClient = useQueryClient()
+
+    const deletar = useMutation((uuidRegistro: string) => deletarRegistro(uuidRegistro), {
+        onSuccess: () => {
+            toast.success(props.textosDeletar?.sucesso, DEFAULT_TOAST_CONFIG)
+            queryClient.invalidateQueries([props.query])
+        },
+    })
     return (
         <div id="card" className="w-full h-96 bg-orange-400 rounded-md overflow-y-auto">
             <div id="card-header" className="bg-orange-700 rounded-md text-center font-bold py-1 sticky top-0">
                 {props.titulo}
             </div>
 
-            <div id="corpo-card" className="p-4">
+            <div id="corpo-card" className="p-8">
                 {
                     Object.keys(props.dados).map((chave: string, index: number) => (
                         <div key={chave} className="flex flex-col mb-4">
                             <b>{chave}</b>
-
                             <div className="w-full p-1 bg-orange-700 rounded-lg mb-1" />
                             <span>{Object.values(props.dados)[index] as string}</span>
                         </div>
@@ -32,7 +45,7 @@ export function CardAreaEntrega(props: {
             <div id="rodape-card" className="p-4 bg-orange-700 sticky bottom-0">
                 <div className="w-full flex justify-center gap-6">
                     <Button className="btn-l font-bold" title={"Editar"} onClick={() => props.onEditar()} />
-                    <Button className="btn-l font-bold" title={"Deletar"} onClick={() => deletarRegistro(props.uuid)} />
+                    <Button className="btn-l font-bold" title={"Deletar"} onClick={() => deletar.mutate(props.uuid)} />
                 </div>
             </div>
 
