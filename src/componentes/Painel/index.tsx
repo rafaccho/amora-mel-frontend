6612f46cb745c36873efbbf7b9d13508194d98cf
Davi Rafacho <davi.s.rafacho.developer@gmail.com>
2022-08-07 00:34:11 -0300

    Adição da dinamicidade no componente de painel

--- src/componentes/Painel/index.tsx
+++ src/componentes/Painel/index.tsx
@@ -21,17 +21,8 @@ return (
         <div id="registros" className="col-span-1">
 
             <Grid
-                exibicaoDadosConfig={[
-                    { coluna: 'Codigo', chaveApi: 'codigo' },
-                    { coluna: 'Nome', chaveApi: 'nome' },
-                    { coluna: 'Unidade 1', chaveApi: 'unidade1' },
-                    { coluna: 'Quantidade 1', chaveApi: 'quantidade1' },
-                    { coluna: 'Unidade 2', chaveApi: 'unidade2' },
-                    { coluna: 'Quantidade 2', chaveApi: 'quantidade2' },
-                ]}
-                requisicaoConfig={{
-                    endpoint: 'produtos',
-                }}
+                exibicaoDadosConfig={props.grid.exibicaoDadosConfig}
+                requisicaoConfig={props.grid.requisicaoConfig}
             />
         </div>
 
