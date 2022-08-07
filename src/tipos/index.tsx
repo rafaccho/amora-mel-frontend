cdf0a35c4cf203ca9c2511c4da93979db44faa38
Davi Rafacho <davi.s.rafacho.developer@gmail.com>
2022-08-07 00:06:03 -0300

    Remoção do tipo 'QueryModulo'

--- src/tipos/index.tsx
+++ src/tipos/index.tsx
@@ -5,4 +5,3 @@ export type UnidadeLoja = 'FA' | 'CN' | 'UN' | 'FL'
 export type SimNao = 'S' | 'N'
 export type Estado = 'RO' | 'AC' | 'AM' | 'RR' | 'PA' | 'AP' | 'TO' | 'MA' | 'PI' | 'CE' | 'RN' | 'PB' | 'PE' | 'AL' | 'SE' | 'BA' | 'MG' | 'ES' | 'RJ' | 'SP' | 'PR' | 'SC' | 'RS' | 'MS' | 'MT' | 'GO' | 'DF'
 export type ReponseStatus = 200 | 201 | 400 | 404 | 500
-export type QueryModulo = "areasEntregas" | "produtos" | 'fornecedores'
