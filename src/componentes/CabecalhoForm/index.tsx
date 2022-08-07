fac895da0d37a3b8d66d8bdba35db288e519ac3b
Davi Rafacho <davi.s.rafacho.developer@gmail.com>
2022-08-07 15:36:18 -0300

    Remodelação do componente de cabeçalho de do form

--- src/componentes/CabecalhoForm/index.tsx
+++ src/componentes/CabecalhoForm/index.tsx
@@ -1,12 +1,24 @@
 import { BotoesForm } from "../BotoesForm";
 
-export function CabecalhoForm(props: {title: string}) {
+export function CabecalhoForm(props: {
+    titulo: string;
+    botoesForm: {
+        onSalvar: () => void;
+        onEditar: () => void;
+        onDeletar: () => void;
+    }
+}) {
     return (
-        <div className="p-5">
-                        <h1 className="t-1 mb-8">{props.title}</h1>
+        <div className="header-form">
+            <h1 className="t-1 mb-2">{props.titulo}</h1>
 
-<BotoesForm />
+            <BotoesForm
+                onSalvar={props.botoesForm.onSalvar}
+                onEditar={props.botoesForm.onEditar}
+                onDeletar={props.botoesForm.onDeletar}
+            />
 
+            <div className="mb-8 border-b-4 border-orange-700" />
         </div>
     )
 }
