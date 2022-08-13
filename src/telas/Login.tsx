4f093b15586dbf3d5c3266421d63ff8d15928d8c
Davi Rafacho <davi.s.rafacho.developer@gmail.com>
2022-08-12 23:10:02 -0300

    Implementação da lógica de login

--- src/telas/Login.tsx
+++ src/telas/Login.tsx
@@ -1,17 +1,45 @@
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+
 import Logo from "../fotos/logo-login.png";
 import Loja from "../fotos/amora-mel.jpg";
+import { useBackend } from "../hooks/useBackend";
+import { useValidacao } from "../hooks/useValidacao";
+
 import { Button } from "../tags";
+import { toast } from "react-toastify";
+import { DEFAULT_TOAST_CONFIG } from "../constantes";
 
 export function Login() {
+    const [usuario, setUsuario] = useState("")
+    const [senha, setSenha] = useState("")
+
+    const txtUsuario = useRef<HTMLInputElement>(null)
+    const txtSenha = useRef<HTMLInputElement>(null)
+
     const navigate = useNavigate()
+    const { login } = useBackend("login")
+    const { validarCampo } = useValidacao()
+
+    function logar() {
+        validarCampo(txtUsuario.current)
+        validarCampo(txtSenha.current)
+
+        !document.querySelector('.invalidado') ? login({ usuario, senha }).then(res =>
+            res.status === 200
+                ? navigate("/")
+                : toast.error("Usuário ou senha inválidos", DEFAULT_TOAST_CONFIG)
+        )
+            : toast.error("Preencha todos os campos", DEFAULT_TOAST_CONFIG)
+    }
+
     return (
         <div className='w-screen h-screen bg-stone-800'>
             <div className="flex flex-wrap">
 
                 <div className="w-full h-screen md:w-1/3 lg:w-1/6 bg-[#f79937] p-2">
 
-                <div className="p-5 mb-14 md:mb-2 flex justify-center">
+                    <div className="p-5 mb-14 md:mb-2 flex justify-center">
                         <img src={Logo} alt="Logo da Amora & Mel" />
                     </div>
 
@@ -22,24 +50,34 @@ export function Login() {
 
                     <div className="p-3">
                         <div className="mb-3">
-                            <label htmlFor="email">Email</label>
-                            <input type="email" id="email" aria-describedby="emailHelp" />
+                            <label htmlFor="Usuario">Usuário</label>
+                            <input type="Usuario" id="Usuario" required
+                                ref={txtUsuario}
+                                value={usuario}
+                                onChange={e => setUsuario(e.target.value)}
+                                onKeyDown={e => e.key === "Enter" && logar()}
+                            />
                         </div>
 
                         <div className="mb-3">
                             <label htmlFor="senha">Senha</label>
-                            <input type="password" id="senha" />
+                            <input type="password" id="senha" required
+                                ref={txtSenha}
+                                value={senha}
+                                onChange={e => setSenha(e.target.value)}
+                                onKeyDown={e => e.key === "Enter" && logar()}
+                            />
                         </div>
 
                         <div className="flex justify-center w-full mt-12">
-                            <Button onClick={() => navigate("/app")} className="btn-l w-full" title="Login" />
+                            <Button onClick={() => logar()} className="btn-l w-full" title="Login" />
                         </div>
                     </div>
 
                 </div>
 
                 <div className="w-full md:w-2/3 lg:w-5/6 md:h-screen">
-                    <img src={Loja} alt="Loja da Amora & Mel" className="w-full h-screen"/>
+                    <img src={Loja} alt="Loja da Amora & Mel" className="w-full h-screen" />
                 </div>
 
             </div>
