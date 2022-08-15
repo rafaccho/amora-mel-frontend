004cdc4ece7354cb015f8f173a91d1ebba9211a1
Davi Rafacho <davi.s.rafacho.developer@gmail.com>
2022-08-15 09:14:53 -0300

    Renomeação da props de title para titulo

--- src/tags/index.tsx
+++ src/tags/index.tsx
@@ -1,10 +1,10 @@
 import { motion } from "framer-motion"
 
-export function Button(props: {title: any, onClick?: () => void, className?: string}) {
+export function Button(props: {titulo: any, onClick?: () => void, className?: string}) {
   return (
     <motion.button className={props.className} onClick={props.onClick}
       whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.92 }} >
-      {props.title}
+      {props.titulo}
     </motion.button>
   )
 }
