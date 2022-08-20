fd5321976c6bcd4b7a8298c28c250315c16d6602
Davi Rafacho <davi.s.rafacho.developer@gmail.com>
2022-08-20 12:44:36 -0300

    Reestruturação do tipo PedidoStatus e renomeação do ResponseCodigoStatus

--- src/tipos/index.tsx
+++ src/tipos/index.tsx
@@ -1,7 +1,7 @@
 export type MetodoHttp = 'get' | 'post' | 'put' | 'delete' | 'patch'
 export type Endpoint = 'pedidos' | 'pedidos_itens' | 'areas_entregas' | 'fornecedores' | 'lojas' | 'produtos' | 'produto_fornecedores' | 'compras' | 'areas_entregas_produto' | 'agrupamentos' | 'estoques' | 'login'
-export type PedidoStatus = 'A' | 'P' | 'B' | 'F' | 'C' 
+export type PedidoStatus = 'A' | 'E' | 'F' | 'C' 
 export type UnidadeLoja = 'FA' | 'CN' | 'UN' | 'FL'
 export type SimNao = 'S' | 'N'
 export type Estado = 'RO' | 'AC' | 'AM' | 'RR' | 'PA' | 'AP' | 'TO' | 'MA' | 'PI' | 'CE' | 'RN' | 'PB' | 'PE' | 'AL' | 'SE' | 'BA' | 'MG' | 'ES' | 'RJ' | 'SP' | 'PR' | 'SC' | 'RS' | 'MS' | 'MT' | 'GO' | 'DF'
-export type ReponseStatus = 200 | 201 | 400 | 404 | 500
+export type ReponseCodigoStatus = 200 | 201 | 400 | 404 | 500
