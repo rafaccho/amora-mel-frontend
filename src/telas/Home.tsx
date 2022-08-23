2f54b14194536375e0e3f069ced9cac4b8bea81b
Davi Rafacho <davi.s.rafacho.developer@gmail.com>
2022-08-23 07:02:53 -0300

    Adição da identação

--- src/telas/Home.tsx
+++ src/telas/Home.tsx
@@ -301,6 +301,7 @@ export function Home() {
 
                                         </table>
                                     </div>
+
                                 </div>
 
                             </div>
